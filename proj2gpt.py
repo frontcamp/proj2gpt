f754fb0fbea14bbb7d58839fef8d8877e4c168d4
Maxim P <maxim.plaksin@gmail.com>
2025-10-21 00:47:40 +0200

    Initial script creation

---
+++ proj2gpt.py
@@ -0,0 +1,10 @@
+
+
+print("""proj2gpt 0.1.0
+Pack project text sources into TXT containers for ChatGPT.
+Copyright (C) 2025 Maksym Plaksin <maxim.plaksin@gmail.com>
+https://github.com/frontcamp/proj2gpt
+---""")
+
+
+
